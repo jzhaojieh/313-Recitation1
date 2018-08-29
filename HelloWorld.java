de01750ee4b572150948d1a28d546200f4924ab9
Jeremy Xue <jhxue@andrew.cmu.edu>
2018-08-29 13:04:31 -0400

    Edited helloworld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello World, Joshua has been at CMU for 3 years");
+        System.out.println("Hello CMU, Joshua has been at World for 20 years");
     }
 
 }
